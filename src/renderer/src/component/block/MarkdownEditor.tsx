0e9b20877a5943f118179ae6f4aa404de7c904cc
Jingyu Xiang <xjyhenry@gmail.com>
2024-01-23 00:27:39 -0500

    fix editor styles

--- src/renderer/src/component/block/MarkdownEditor.tsx
+++ src/renderer/src/component/block/MarkdownEditor.tsx
@@ -1,4 +1,6 @@
 import "@mdxeditor/editor/style.css"
+import "./styles.css"
+
 import {
   MDXEditor,
   headingsPlugin,
@@ -9,7 +11,8 @@ import {
 
 export function MarkdownEditor(): JSX.Element {
   const classname =
-    "outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
+    "outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-[''] text-white!"
+
   return (
     <MDXEditor
       markdown={"## HI"}
@@ -19,6 +22,7 @@ export function MarkdownEditor(): JSX.Element {
         quotePlugin(),
         markdownShortcutPlugin(),
       ]}
+      className="dark-editor"
       contentEditableClassName={classname}
     />
   )
